531ec3589d7044d9d76ea974bee6d8ed2df515b3
slaven89 <kslav002@gmail.com>
2022-03-24 21:56:11 -0400

    finished login page, have e-mail page next before working on CSS

--- login.js
+++ login.js
@@ -3,6 +3,7 @@ let loginPassword = document.getElementById('loginPassword');
 let viewPass = document.querySelector('.viewPass');
 let loginSubmit = document.getElementById('loginSubmit');
 
+
 var signupInfo = sessionStorage.getItem('signupInfo');
 var signupArray = JSON.parse(signupInfo);
 
@@ -31,6 +32,28 @@ var passwordValidate = (ev) => {
     }
 }
 
+var submitSearch = (ev) => {
+    ev.preventDefault();
+    let gotWrong = document.getElementById('gotWrong');
+    let fieldOne = loginEmail.value;
+    let fieldTwo = loginPassword.value;
+    for (start = 0; start < signupArray.length; start++) {
+        if (fieldOne == signupArray[start].email) {
+            if (fieldTwo == signupArray[start].password) {
+                window.location = 'index.html';
+            } else {
+                document.getElementById('loginEmail').value = null;
+                document.getElementById('loginPassword').value = null;
+                gotWrong.hidden = false;
+            }
+        }
+    };
+    alert('E-mail not found in our records');
+    document.getElementById('loginEmail').value = null;
+    document.getElementById('loginPassword').value = null;
+    gotWrong.hidden = false;
+}
+
 viewPass.addEventListener('click' , () => {
     if (loginPassword.type === "password") {
         loginPassword.type = "text";
@@ -47,4 +70,6 @@ viewPass.addEventListener('click' , () => {
         loginPassword.addEventListener('keypress', passwordValidate);
     });
     
-
+    document.addEventListener('DOMContentLoaded', () => {
+        loginSubmit.addEventListener('click', submitSearch);
+    });
